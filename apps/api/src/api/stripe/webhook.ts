cdb589ef7140f752a18830fa29d190480e5d5204
Grant Forrest <gaforres@gmail.com>
2022-09-05 16:44:04 -0400

    add error and response handling to stripe

--- apps/api/src/api/stripe/webhook.ts
+++ apps/api/src/api/stripe/webhook.ts
@@ -25,79 +25,87 @@ export async function webhookHandler(req: Request, res: Response) {
 	let status;
 	let planId;
 
-	switch (event.type) {
-		case 'customer.subscription.trial_will_end':
-			subscription = event.data.object as Stripe.Subscription;
-			status = subscription.status;
-			planId = subscription.metadata.planId;
-			if (!planId) {
-				console.error('No planId found on subscription ' + subscription.id);
+	try {
+		switch (event.type) {
+			case 'customer.subscription.trial_will_end':
+				subscription = event.data.object as Stripe.Subscription;
+				status = subscription.status;
+				planId = subscription.metadata.planId;
+				if (!planId) {
+					console.error('No planId found on subscription ' + subscription.id);
+					break;
+				}
+				await prisma.plan.update({
+					where: { id: planId },
+					data: { subscriptionStatus: status },
+				});
 				break;
-			}
-			await prisma.plan.update({
-				where: { id: planId },
-				data: { subscriptionStatus: status },
-			});
-			break;
-		case 'customer.subscription.deleted':
-			subscription = event.data.object as Stripe.Subscription;
-			status = subscription.status;
-			planId = subscription.metadata.planId;
-			if (!planId) {
-				console.error('No planId found on subscription ' + subscription.id);
-				break;
-			}
-			await prisma.plan.update({
-				where: { id: planId },
-				data: {
-					subscriptionStatus: status,
-					subscriptionCanceledAt: subscription.canceled_at
-						? new Date(subscription.canceled_at * 1000)
-						: null,
-				},
-			});
-			break;
-		case 'customer.subscription.created':
-			subscription = event.data.object as Stripe.Subscription;
-			status = subscription.status;
-			planId = subscription.metadata.planId;
-			if (!planId) {
-				console.error('No planId found on subscription ' + subscription.id);
-				break;
-			}
-			await prisma.plan.update({
-				where: { id: planId },
-				data: {
-					subscriptionStatus: status,
-					stripeSubscriptionId: subscription.id,
-					stripeCustomerId: subscription.customer as string,
-				},
-			});
-			break;
-		case 'customer.subscription.updated':
-			subscription = event.data.object as Stripe.Subscription;
-			status = subscription.status;
-			planId = subscription.metadata.planId;
-			if (!planId) {
-				console.error('No planId found on subscription ' + subscription.id);
-				break;
-			}
-			await prisma.plan.update({
-				where: { id: planId },
-				data: {
-					subscriptionStatus: status,
-					stripeSubscriptionId: subscription.id,
-					subscriptionCanceledAt: subscription.canceled_at
-						? new Date(subscription.canceled_at * 1000)
-						: null,
-					subscriptionExpiresAt:
-						subscription.status !== 'active' &&
-						subscription.status !== 'trialing'
-							? new Date(subscription.current_period_end * 1000)
+			case 'customer.subscription.deleted':
+				subscription = event.data.object as Stripe.Subscription;
+				status = subscription.status;
+				planId = subscription.metadata.planId;
+				if (!planId) {
+					console.error('No planId found on subscription ' + subscription.id);
+					break;
+				}
+				await prisma.plan.update({
+					where: { id: planId },
+					data: {
+						subscriptionStatus: status,
+						subscriptionCanceledAt: subscription.canceled_at
+							? new Date(subscription.canceled_at * 1000)
 							: null,
-					stripeCustomerId: subscription.customer as string,
-				},
-			});
-			break;
+					},
+				});
+				break;
+			case 'customer.subscription.created':
+				subscription = event.data.object as Stripe.Subscription;
+				status = subscription.status;
+				planId = subscription.metadata.planId;
+				if (!planId) {
+					console.error('No planId found on subscription ' + subscription.id);
+					break;
+				}
+				await prisma.plan.update({
+					where: { id: planId },
+					data: {
+						subscriptionStatus: status,
+						stripeSubscriptionId: subscription.id,
+						stripeCustomerId: subscription.customer as string,
+					},
+				});
+				break;
+			case 'customer.subscription.updated':
+				subscription = event.data.object as Stripe.Subscription;
+				status = subscription.status;
+				planId = subscription.metadata.planId;
+				if (!planId) {
+					console.error('No planId found on subscription ' + subscription.id);
+					break;
+				}
+				await prisma.plan.update({
+					where: { id: planId },
+					data: {
+						subscriptionStatus: status,
+						stripeSubscriptionId: subscription.id,
+						subscriptionCanceledAt: subscription.canceled_at
+							? new Date(subscription.canceled_at * 1000)
+							: null,
+						subscriptionExpiresAt:
+							subscription.status !== 'active' &&
+							subscription.status !== 'trialing'
+								? new Date(subscription.current_period_end * 1000)
+								: null,
+						stripeCustomerId: subscription.customer as string,
+					},
+				});
+				break;
+		}
+	} catch (err) {
+		console.error('!!! Stripe webhook error');
+		console.error(err);
+		res.status(500).send('Error processing webhook');
 	}
+
+	res.sendStatus(200);
 }
