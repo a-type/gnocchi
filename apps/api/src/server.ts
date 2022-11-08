6c6e3c7815de23505274df8e1cef4de0867e66a6
Grant Forrest <gaforres@gmail.com>
2022-11-08 17:38:40 -0500

    add cors origin

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -17,6 +17,8 @@ app.use(
 			'http://localhost:3000',
 			'http://127.0.0.1:3000',
 			'https://aglio.gfor.rest',
+			'https://aglio.app',
+			'https://www.aglio.app',
 		],
 		credentials: true,
 	}),
