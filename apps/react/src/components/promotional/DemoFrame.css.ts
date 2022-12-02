ebefbb6fc8490e93f630421612dc325026981c23
Grant Forrest <gaforres@gmail.com>
2022-12-02 16:24:14 -0500

    tweak demo styles

--- apps/react/src/components/promotional/DemoFrame.css.ts
+++ apps/react/src/components/promotional/DemoFrame.css.ts
@@ -4,12 +4,12 @@ import { style } from '@vanilla-extract/css';
 export const root = style({
 	display: 'flex',
 	flexDirection: 'column',
-	border: `6px solid ${vars.colors.black}`,
+	border: `1px solid ${vars.colors.black}`,
 	borderRadius: vars.radii.lg,
+	overflow: 'hidden',
 });
 
 export const image = style({
 	width: '100%',
 	height: 'auto',
-	borderRadius: vars.radii.lg,
 });
