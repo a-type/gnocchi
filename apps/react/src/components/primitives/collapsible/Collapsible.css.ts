0bfe760382b4f8a9d72761495df87d4b62c17221
Grant Forrest <gaforres@gmail.com>
2022-12-24 11:21:25 -0500

    tweak some spacing

---
+++ apps/react/src/components/primitives/collapsible/Collapsible.css.ts
@@ -0,0 +1,39 @@
+import { keyframes, style } from '@vanilla-extract/css';
+
+const openVertical = keyframes({
+	from: { height: 0 },
+	to: { height: 'var(--radix-collapsible-content-height)' },
+});
+const closeVertical = keyframes({
+	from: { height: 'var(--radix-collapsible-content-height)' },
+	to: { height: 0 },
+});
+const openHorizontal = keyframes({
+	from: { width: 0 },
+	to: { width: 'var(--radix-collapsible-content-width)' },
+});
+const closeHorizontal = keyframes({
+	from: { width: 'var(--radix-collapsible-content-width)' },
+	to: { width: 0 },
+});
+
+export const content = style({
+	overflow: 'hidden',
+	animationTimingFunction: '$transitions$springy',
+	selectors: {
+		'&[data-state=open]': {
+			animationName: openVertical,
+			animationDuration: '300ms',
+		},
+		'&[data-state=closed]': {
+			animationName: closeVertical,
+			animationDuration: '300ms',
+		},
+		'&[data-state=open][data-horizontal]': {
+			animationName: openHorizontal,
+		},
+		'&[data-state=closed][data-horizontal]': {
+			animationName: closeHorizontal,
+		},
+	},
+});
