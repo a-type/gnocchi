676ae05a21b83d4748700eb5dcf64c459754993b
Grant Forrest <gaforres@gmail.com>
2023-01-11 12:31:34 -0500

    fix menu for non-auth users

--- apps/react/src/pages/PlanPage.tsx
+++ apps/react/src/pages/PlanPage.tsx
@@ -9,30 +9,42 @@ import {
 import { InviteLinkButton } from '@/components/sync/InviteLinkButton.js';
 import { LogoutButton } from '@/components/auth/LogoutButton.js';
 import { API_HOST_HTTP } from '@/config.js';
-import { ArrowLeftIcon } from '@radix-ui/react-icons';
+import { ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons';
 import { Link } from 'react-router-dom';
 import { ManageCategoriesDialog } from '@/components/menu/ManageCategoriesDialog.jsx';
 import { BugButton } from '@/components/menu/BugButton.jsx';
+import { ReactNode } from 'react';
+import { useAuth } from '@/contexts/AuthContext.jsx';
+import { useInterval } from '@/hooks/useInterval.js';
+import { LoginButton } from '@/components/sync/LoginButton.jsx';
+
+const contents = {
+	offline: OfflineContents,
+	anonymous: AnonymousContents,
+	unsubscribed: UnsubscribedContents,
+	online: OnlineContents,
+} as const;
 
 export function PlanPage() {
+	const { session, isSubscribed, error } = useAuth();
+
+	let state: keyof typeof contents = 'online';
+	if (error) {
+		state = 'offline';
+	} else if (!session) {
+		state = 'anonymous';
+	} else if (!isSubscribed) {
+		state = 'unsubscribed';
+	}
+
+	const Contents = contents[state];
+
 	return (
 		<PageRoot>
 			<PageContent fullHeight noPadding>
 				<Box width="full" direction="column" mt={6} p={4} gap={4} align="start">
 					<H1>Settings</H1>
-					<div>
-						<InviteLinkButton color="primary" />
-						<Span size="xs">Get a link to invite others to your list</Span>
-					</div>
-					<ManageSubscriptionButton />
-					<div>
-						<ManageCategoriesDialog>
-							<Button>Manage categories</Button>
-						</ManageCategoriesDialog>
-						<Span size="xs">Add, remove, and rearrange categories</Span>
-					</div>
-					<BugButton />
-					<LogoutButton>Log out</LogoutButton>
+					<Contents />
 				</Box>
 			</PageContent>
 		</PageRoot>
@@ -47,5 +59,91 @@ function ManageSubscriptionButton(props: ButtonProps) {
 			</Button>
 			<Span size="xs">Update your card or unsubscribe</Span>
 		</form>
+	);
+}
+
+const MainContainer = ({ children }: { children: ReactNode }) => (
+	<Box width="full" direction="column" gap={4} align="start">
+		{children}
+	</Box>
+);
+
+function OfflineContents() {
+	const { refetch } = useAuth();
+
+	useInterval(refetch, 3000);
+
+	return (
+		<MainContainer>
+			<Button size="small" color="default" onClick={refetch}>
+				Retry connection
+			</Button>
+			<ManageCategories />
+			<BugButton />
+		</MainContainer>
+	);
+}
+
+function AnonymousContents() {
+	return (
+		<MainContainer>
+			<LoginButton color="primary" returnTo="/">
+				<ArrowRightIcon />
+				&nbsp;Subscribe (or sign in)
+			</LoginButton>
+			<ManageCategories />
+			<BugButton />
+		</MainContainer>
+	);
+}
+
+function UnsubscribedContents() {
+	return (
+		<MainContainer>
+			<Box
+				background="primaryWash"
+				color="primaryDarker"
+				p={4}
+				borderRadius="lg"
+			>
+				Subscription inactive
+			</Box>
+			<ManageSubscriptionButton />
+			<ManageCategories />
+			<LogoutButton>Sign out</LogoutButton>
+			<BugButton />
+		</MainContainer>
+	);
+}
+
+function OnlineContents() {
+	return (
+		<MainContainer>
+			<Invite />
+			<ManageCategories />
+			<ManageSubscriptionButton />
+			<BugButton />
+			<LogoutButton>Sign out</LogoutButton>
+		</MainContainer>
+	);
+}
+
+function ManageCategories() {
+	return (
+		<div>
+			<ManageCategoriesDialog>
+				<Button>Manage categories</Button>
+			</ManageCategoriesDialog>
+			<Span size="xs">Add, remove, and rearrange categories</Span>
+		</div>
+	);
+}
+
+function Invite() {
+	return (
+		<div>
+			<InviteLinkButton color="primary" />
+			<Span size="xs">Get a link to invite others to your list</Span>
+		</div>
 	);
 }
