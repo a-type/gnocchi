72d358a618f83f440884264c96bf85d314126480
Grant Forrest <gaforres@gmail.com>
2023-02-21 20:12:50 -0500

    fix prelude spacing

---
+++ apps/hub/components/Prelude.css.ts
@@ -0,0 +1,6 @@
+import { vars } from '@/../../packages/ui/src';
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+	paddingBottom: vars.space[4],
+});
--- apps/hub/components/Prelude.tsx
+++ apps/hub/components/Prelude.tsx
@@ -2,6 +2,8 @@ import { H4, H2, H3, P, Peek, RichEditor } from '@aglio/ui';
 import { HubPublishedRecipeInfo } from '@aglio/trpc';
 import { useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import classNames from 'classnames';
+import * as classes from './Prelude.css.js';
 
 export interface PreludeProps {
 	content: Exclude<HubPublishedRecipeInfo['prelude'], null>;
@@ -15,7 +17,10 @@ export function Prelude({ content }: PreludeProps) {
 	});
 	return (
 		<Peek peekHeight={400}>
-			<div className="p-summary" itemProp="description">
+			<div
+				className={classNames('p-summary', classes.container)}
+				itemProp="description"
+			>
 				<RichEditor editor={editor} readOnly />
 			</div>
 		</Peek>
