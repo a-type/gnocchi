e2a6b53c1846e8410095cf22d426c06238a2a310
Grant Forrest <gaforres@gmail.com>
2023-03-28 14:30:48 -0400

    attempt to respect robots.txt for scanner

--- packages/scanning/src/index.ts
+++ packages/scanning/src/index.ts
@@ -1,8 +1,26 @@
 export type { ExtractorData } from './extractors/types.js';
 import cheerio from 'cheerio';
 import { extract } from './extractor.js';
+import { default as robotsParser } from 'robots-parser';
 
 export async function scanWebRecipe(siteUrl: string) {
+	const siteAsUrl = new URL(siteUrl);
+	const robotsUrl = `${siteAsUrl.origin}/robots.txt`;
+	const robotsFileResponse = await fetch(robotsUrl);
+	if (robotsFileResponse.status !== 404) {
+		const robotsFileText = await robotsFileResponse.text();
+		try {
+			// @ts-ignore
+			const robots = await robotsParser(robotsUrl, robotsFileText);
+
+			if (robots?.isDisallowed(siteUrl, 'Gnocchibot')) {
+				return null;
+			}
+		} catch (e) {
+			console.error('Could not parse robots.txt', e);
+		}
+	}
+
 	const response = await fetch(siteUrl, {
 		headers: {
 			'User-Agent':
