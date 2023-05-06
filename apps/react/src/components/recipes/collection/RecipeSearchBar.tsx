0da3b48f155de02f6184c7fea7001c77d867f936
Grant Forrest <gaforres@gmail.com>
2023-05-06 17:19:00 -0400

    make recipe search clearable

--- apps/react/src/components/recipes/collection/RecipeSearchBar.tsx
+++ apps/react/src/components/recipes/collection/RecipeSearchBar.tsx
@@ -1,15 +1,25 @@
 import { useRecipeTitleFilter } from '@/components/recipes/collection/hooks.js';
+import { Box } from '@aglio/ui/src/components/box';
+import { Button } from '@aglio/ui/src/components/button';
 import { LiveUpdateTextField } from '@aglio/ui/src/components/liveUpdateTextField';
+import { Cross2Icon } from '@radix-ui/react-icons';
 
 export function RecipeSearchBar(props: { className?: string }) {
 	const [value, setValue] = useRecipeTitleFilter();
 
 	return (
-		<LiveUpdateTextField
-			placeholder="Search recipes"
-			value={value}
-			onChange={setValue}
-			{...props}
-		/>
+		<Box direction="row" gap={3}>
+			<LiveUpdateTextField
+				placeholder="Search recipes"
+				value={value}
+				onChange={setValue}
+				{...props}
+			/>
+			{!!value && (
+				<Button size="icon" color="ghost" onClick={() => setValue('')}>
+					<Cross2Icon />
+				</Button>
+			)}
+		</Box>
 	);
 }
