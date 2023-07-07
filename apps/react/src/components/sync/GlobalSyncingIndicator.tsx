9749e7a67e2d2d0e3cfa30c3e5949472bd360f29
Grant Forrest <gaforres@gmail.com>
2023-07-07 11:21:03 -0400

    improve sync indicator

--- apps/react/src/components/sync/GlobalSyncingIndicator.tsx
+++ apps/react/src/components/sync/GlobalSyncingIndicator.tsx
@@ -1,5 +1,6 @@
 import { hooks } from '@/stores/groceries/index.js';
 import { ReloadIcon } from '@radix-ui/react-icons';
+import classNames from 'classnames';
 import { useEffect, useState } from 'react';
 
 export interface GlobalSyncingIndicatorProps {}
@@ -11,14 +12,18 @@ export function GlobalSyncingIndicator({}: GlobalSyncingIndicatorProps) {
 		return client.sync.subscribe('syncingChange', setSyncing);
 	}, [client]);
 
-	if (syncing) {
-		return (
-			<div className="fixed top-2 right-2 z-tooltip bg-gray-1 rounded-full p-1 text-xs flex flex-row gap-1 items-center animate-fade-in animate-delay-500">
-				<ReloadIcon className="animate-spin" />
-				<span>Syncing</span>
-			</div>
-		);
-	}
-
-	return null;
+	return (
+		<div
+			className={classNames(
+				'fixed top-2 right-2 z-tooltip bg-gray-1 rounded-full p-1 text-xs flex flex-row gap-1 items-center transition-opacity transition-delay-500 opacity-0',
+				{
+					'opacity-100': syncing,
+				},
+			)}
+			aria-hidden={!syncing}
+		>
+			<ReloadIcon className="animate-spin" />
+			<span>Syncing</span>
+		</div>
+	);
 }
