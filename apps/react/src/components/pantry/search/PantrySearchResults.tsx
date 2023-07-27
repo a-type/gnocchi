f9b273288ecf1f790bec6d1339a20436797288ff
Grant Forrest <gaforres@gmail.com>
2023-07-27 10:34:46 -0400

    make search results fullwidth

--- apps/react/src/components/pantry/search/PantrySearchResults.tsx
+++ apps/react/src/components/pantry/search/PantrySearchResults.tsx
@@ -22,7 +22,7 @@ export function PantrySearchResults({}: PantrySearchResultsProps) {
 
 	return (
 		<div className="flex flex-col gap-4 items-center">
-			<CardGrid className="grid-cols-[repeat(2,1fr)]">
+			<CardGrid className="grid-cols-[repeat(2,1fr)] w-full">
 				{results.map((item) => {
 					return <PantryListItem key={item.get('canonicalName')} item={item} />;
 				})}
