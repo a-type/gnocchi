65b5fc1931f199c6e589183b5a47ba06a4fda12b
Grant Forrest <gaforres@gmail.com>
2024-03-01 23:51:32 -0500

    exclude temperatures from multiplication

--- apps/react/src/components/recipes/viewer/TextWithMultipliedNumbers.tsx
+++ apps/react/src/components/recipes/viewer/TextWithMultipliedNumbers.tsx
@@ -6,21 +6,27 @@ export interface TextWithMultipliedNumbersProps {
 	multiplier: number;
 }
 
+// This regex matches any number with a decimal point, or any number with a fraction.
+// however, if the number ends with a degree symbol, or "F" or "C", it does not match
+// any part of it. for example, "350F" would not match, but "350" would.
+// but also "350F" would not match "35"... but also "45cans" would match.
+const numberRegex = /(\d+\.\d+|\d+\/\d+|\d+)(?!\d*\s*[FC°])/g;
+
 export function TextWithMultipliedNumbers({
 	text,
 	multiplier,
 }: TextWithMultipliedNumbersProps) {
 	if (multiplier === 1) return <>{text}</>;
 	if (!text) return <>{text}</>;
-	// This regex matches any number with a decimal point, or any number with a fraction
-	const numberRegex = /(\d+\.\d+|\d+\/\d+|\d+)/g;
+
 	const matches = text.match(numberRegex);
 	if (!matches) return <>{text}</>;
 	const fragments = text.trim().split(numberRegex);
+	console.log(fragments);
 	return (
 		<>
 			{fragments.map((fragment, index) => {
-				const isNumber = /(\d+\.\d+|\d+\/\d+|\d+)/.test(fragment);
+				const isNumber = numberRegex.test(fragment);
 				return (
 					<span key={index}>
 						{!isNumber && fragment}
@@ -33,7 +39,7 @@ export function TextWithMultipliedNumbers({
 								}
 							>
 								<span className="multiplied-number text-accent-dark font-bold flex-row inline-flex items-center gap-0.5">
-									{parseFloat(fragment) * multiplier}
+									{parseFloat(fragment.trim()) * multiplier}
 								</span>
 							</Tooltip>
 						)}
